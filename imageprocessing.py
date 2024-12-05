781f591e7475c90fbf6219d4b15b56df2519eca6
santiagohopkins <santiagoahopkins@gmail.com>
2024-12-04 19:32:35 -0800

    added nearest neighbors funciton, added mapping to real lenght coords

--- imageprocessing.py
+++ imageprocessing.py
@@ -6,8 +6,12 @@ class Image:
     def __init__(self, path_to_image):
         # Read the image and detect edges
         self.image = cv.imread(path_to_image)
+        self.image_height = self.image.shape[0]
+        self.image_width = self.image.shape[1]
         self.gray = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
         self.waypoints = None
+        self.canvas_height = 1.0 # in meters
+        self.canvas_width = 1.0 # also in meters 
 
     def getWaypoints(self):
         # Apply Gaussian blur to reduce noise
@@ -27,7 +31,18 @@ class Image:
             approx = cv.approxPolyDP(contour, epsilon, True)
             self.waypoints.extend(approx.reshape(-1, 2))
 
+
         return self.waypoints
+    
+    def pixels_to_xy(self):
+        pixel2xy = self.canvas_width / self.image_width
+        xy_waypoints = []
+        for point in self.waypoints:
+            xy_point = point * pixel2xy
+            xy_waypoints.append(xy_point)
+        self.waypoints = np.array(xy_waypoints)
+        return self.waypoints
+
 
 # Create a figure and axis
     def plot(self):
@@ -56,8 +71,9 @@ class Image:
         return ax
     
 def main():
-    img = Image('images/smiley1.jpg')
+    img = Image('images/square.jpg')
     img.getWaypoints()
+    img.pixels_to_xy()
     plot = img.plot()
     pass
     
