44565d4f2f6ac0f5b7c44bb7121fd317c4d5f5e0
danielmunicio <danielmunicio360@gmail.com>
2024-12-20 12:43:06 -0800

    Cleaned up Microcontroller bits and bobs

--- microcontroller/microcontroller/main.py
+++ microcontroller/microcontroller/main.py
@@ -1,18 +1,14 @@
 import rclpy
-import numpy as np
 import re
 from rclpy.node import Node
 import serial
-from sensor_msgs.msg import Imu
-from std_msgs.msg import Float64
-from geometry_msgs.msg import TwistStamped
 from drone_msgs.msg import MotorCommand, DistanceSensorArray, SensorMessage
 import math
+import numpy as np
 
 class Arduino(Node):
     def __init__(self):
         super().__init__('arduino_connection_node')
-        print("here")
         self.control_subscriber = self.create_subscription(
             MotorCommand,
             '/cmd',
@@ -32,50 +28,33 @@ class Arduino(Node):
             1
         )
         
-        # Initialize controls
-        self.acceleration = 0
-        self.steering_angle = 0 
         self.serial_port = '/dev/serial/by-id/usb-Arduino_LLC_Arduino_MKRZero_C4260BF85030574B412E3120FF13161F-if00'
         self.baud_rate = 115200
         self.ser = serial.Serial(self.serial_port, self.baud_rate)
 
         # Create a timer that goes off every 0.001 seconds, and calls self.recieve_serial_values
+        # Created this way because cannot create a while True loop before Node initialization finishes
         self.serial_timer = self.create_timer(0.001, self.recieve_serial_values)
-        self.arduino_initialized = True
 
     def cmd_callback(self, msg: MotorCommand):
-        """ 
-        Callback function that should recieve car commands from MPC.
-        Sends the acceleration and steering values to the arduino through Serial.
-        """
         motor1 = msg.motor1
         motor2 = msg.motor2
         motor3 = msg.motor3
         motor4 = msg.motor4
-
-        print("Recieved Message")
-        #Convert the steering value to what the motor has to rotate
+        # Write Motor Message that Arduino is expecting
         motor_message = f"{str(msg.motor1) + "/" + str(msg.motor2) + "/" + str(msg.motor3) + "/" + str(msg.motor4) + "/"}" + '\n'
-        print("MOTOR MESSAGE")
         #self.ser.write(str(motor_str).encode('utf-8'))
         #self.ser.write(motor_str.encode())
         self.ser.write(bytearray(motor_message, 'ascii'))
-        print("sent message")
         return 
 
     def recieve_serial_values(self):
-        #Note: This probably isn't the most time efficient method of doing this, but I just wrote this in a day for push-testing
         if self.ser.in_waiting > 0: 
             data = self.ser.readline()
-            print("DATA: ", data)
-            if data == "Initialize finished":
-                self.arduino_initialized = True
-                self.get_logger().info("Arduino initialization complete. Starting motor commands.")
 
-            # Regex for quaternions
+            # Regex for different message patterns
             quaternion_pattern = r"qW: ([\d\.\-]+) qX: ([\d\.\-]+) qY: ([\d\.\-]+) qZ: ([\d\.\-]+)"
             gyroscope_pattern = r"Roll Rate:\s*([-+]?\d*\.\d+|\d+)\s*Pitch Rate:\s*([-+]?\d*\.\d+|\d+)\s*Yaw Rate:\s*([-+]?\d*\.\d+|\d+)"
-            # Regex for depth
             depth_pattern = r"D=(\d+)mm"
 
             # Extract quaternions
@@ -87,7 +66,7 @@ class Arduino(Node):
                 try: 
                     roll, pitch, yaw = self.quat_to_euler((qW, qX, qY, qZ))
                 except:
-                    print("FAILED")
+                    print("Quaternion Conversion Failed")
                     return
                 imu_message = SensorMessage()
                 imu_message.x = 0.0
@@ -106,7 +85,7 @@ class Arduino(Node):
             # Extract depth
             depth_match = re.search(depth_pattern, str(data))
             if depth_match:
-                depth = int(depth_match.group(1))  # Convert depth to integer
+                depth = int(depth_match.group(1))
                 distance_message = DistanceSensorArray()
                 distance_message.x = 0.0
                 distance_message.y = 0.0
@@ -118,7 +97,12 @@ class Arduino(Node):
                 pitch_rate = float(gyroscope_match.group(2))
                 yaw_rate = float(gyroscope_match.group(3))
                 
-    def quat_to_euler(self, quat):#: Quaternion) -> tuple[float, float, float]:
+    def quat_to_euler(self, quat):
+        """
+        Helper function that converts quaternions to roll pitch yaw
+        Pulled from Stack Overflow
+        https://stackoverflow.com/questions/5782658/extracting-yaw-from-a-quaternion
+        """
         x = quat[1]
         y = quat[2]
         z = quat[3]
